c8adb03a8b8cd301c1e0dd5ce96855aeb8d5313e
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2021-09-08 14:42:06 -0700

    changed the is_leap() function

--- Days_in_Months/functions.py
+++ Days_in_Months/functions.py
@@ -1,17 +1,11 @@
 from os import system, name
 
 def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:         
-                leap = True
-            else:
-                leap = False
-        else:
-            leap = True  
-    else:
-        leap = False  
-    return leap
+    if year % 400 == 0:
+        return True
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    return False
 
 def days_in_month(year, month):
     month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
