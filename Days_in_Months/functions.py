6876e68105ce0b54d1f1d9b29108470e13947123
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2021-09-11 19:35:32 -0700

    added docstring messages to the functions

--- Days_in_Months/functions.py
+++ Days_in_Months/functions.py
@@ -1,6 +1,7 @@
 from os import system, name
 
 def is_leap(year):
+    """Checks if a year is a leap year, returns a boolean"""
     if year % 400 == 0:
         return True
     if year % 4 == 0 and year % 100 != 0:
@@ -8,6 +9,7 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
+    """returns the number of days in a given month, in a given year"""
     month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
     leap = is_leap(year)
     number = month_days[month]
@@ -19,11 +21,13 @@ def days_in_month(year, month):
         return number
 
 def month_name(month_num):
+    """Takes a number and returns the corresponding month's name as a string"""
     months_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     the_name = months_names[month_num - 1]
     return the_name
 
 def clear():
+    """Clears the terminal"""
     if name == 'nt':
         _=system('cls')
     else:
